b2b210434d3c20a4d9b94b09475d82308146940b
ginomaglioni <ginomaglioni@institutocedec.com>
2025-04-28 16:51:26 -0300

    03 - Gestión de Datos - 28/04

---
+++ src/app/carrito.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarritoService {
+  items: Product [] =  [];
+
+  constructor(private http: HttpClient) {}
+
+  agregarAlCarrito(producto: Product){
+    this.items.push(producto);
+  }
+
+  getItems(){
+    return this.items;
+  }
+
+  limpiarCarrito(){
+    this.items = [];
+    return this.items;
+  }
+
+  getPrecioDeEnvio(){
+    return this.http.get<{type: string, price: number}[]>
+    ('/assets/shipping.json');
+  }
+}
--- src/app/detalle-producto/detalle-producto.component.ts
+++ src/app/detalle-producto/detalle-producto.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../products';
+import { CarritoService } from '../carrito.service';
 
 @Component({
   selector: 'app-detalle-producto',
@@ -10,13 +11,22 @@ import { Product, products } from '../products';
 export class DetalleProductoComponent implements OnInit {
   producto: Product | undefined;
 
-  constructor(private ruta: ActivatedRoute) {}
+  constructor(
+    private ruta: ActivatedRoute,
+    private carritoService: CarritoService
+ ) {}
+ 
 
   ngOnInit(): void {
     const paramRuta = this.ruta.snapshot.paramMap;
     const idProducto = Number(paramRuta.get('id'));
 
     this.producto = products.find(producto => producto.id === idProducto);
+  }
+
+  agregarAlCarrito(producto: Product){
+    this.carritoService.agregarAlCarrito(producto);
+    window.alert('El producto se ha agregado al carrito')
   }
 
 }
